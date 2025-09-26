6b6e489baed5bc0e0f3b7cf07cfd7c7b4a8ed4f8
Osher David <osherdavid111@gmail.com>
2025-09-26 20:46:16 +0300

    Added mkdir func + bugfix

--- p2p_file_share/commands/mkdir.py
+++ p2p_file_share/commands/mkdir.py
@@ -6,33 +6,33 @@ from p2p_file_share.commands.command import Command
 from p2p_file_share.commands.commands import register_command
 
 
-@register_command("CWD")
-class Cd(Command):
-    """Change the current working directory of the server."""
+@register_command("MKD")
+class Mkdir(Command):
+    """Create a new directory on the  server."""
 
     def execute_server(self, conn, addr, *args, **kwargs):
-        """Handle request to change the current working directory."""
+        """Handle request to create a new directory."""
         requested_directory = conn.recv(self.RECIEVE_BUFFER_SIZE)
-        self.logger.info(f"Requested to change directory to: {requested_directory} by {addr}")
+        self.logger.info(f"Requested to create the directory: {requested_directory} by {addr}")
         try:
-            os.chdir(requested_directory)
+            os.mkdir(requested_directory)
             conn.sendall(self.ACK_STRING)
         except Exception as e:
-            self.logger.error(f"Couldn't change working directory because: {e}")
+            self.logger.error(f"Couldn't create the directory because: {e}")
             conn.sendall(self.ERR_STRING)
 
     def execute_client(self, conn, directory: str, *args, **kwargs):
-        """Send and handle repsponse for the request to change the working directory."""
+        """Send and handle repsponse for the request to create the directory."""
         conn.sendall(directory.encode())
         response = conn.recv(self.RECIEVE_BUFFER_SIZE)
         if response == self.ACK_STRING:
-            typer.secho(f"Server successfuly changed its current working directory to {directory}.",
+            typer.secho(f"Server successfuly created the directory {directory}.",
                         fg=typer.colors.GREEN)
         else:
-            typer.secho(f"Server could not change its working directory to {directory}.",
+            typer.secho(f"Server could not create the directory {directory}.",
                         fg=typer.colors.RED)
 
     @classmethod
     def help(cls) -> str:
         """Return a help string for the command."""
-        return "Request the server to change its current working directory."
+        return "Create a new directory on the remote server."
